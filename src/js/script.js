a24c303fb8adb712d3cf88b6fcafece565cc31e0
Samio Azeredo <s.a.m.i.o@hotmail.com>
2026-05-27 18:14:22 -0300

    Adicionado navegação entre semanas

--- src/js/script.js
+++ src/js/script.js
@@ -7,12 +7,25 @@ const imagens = ['1semana.jpg', '2semana.jpg', '3semana.jpg', '4semana.jpg', '5s
 
 direito.addEventListener('click', function() {
     imagemAtual = imagemAtual +1;
+        if (imagemAtual > 4) {
+            imagemAtual = 4;
+            direito.disabled = true;
+        } else {
+            direito.disabled = false;
+        }
+    esquerdo.disabled = false;    
     img.src = '/src/img/' + imagens[imagemAtual];
     
 })
 
 esquerdo.addEventListener('click', function() {
     imagemAtual = imagemAtual -1;
-    img.src = '/src/img/' + imagens[imagemAtual];
-    
+        if (imagemAtual < 0) {
+            imagemAtual = 0
+            esquerdo.disabled = true;
+        } else {
+            esquerdo.disabled = false;
+        }
+    direito.disabled = false;
+    img.src = '/src/img/' + imagens[imagemAtual];    
 })
